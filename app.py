513a6315b192487a59e91340811e4102180c1457
naya86 <80315799+naya86@users.noreply.github.com>
2021-03-22 17:48:21 +0900

    add

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import requests
+from fbprophet import Prophet
+import string
+import FinanceDataReader as fdr
+import bs4
+from PIL import Image
+from kospi import run_kospi
+from america import run_america
+from search import run_search
+
+
+
+def main() :
+    
+    menu = ['HOME', 'KOSPI & KOSDAQ', 'DJI & NASDAQ','종목검색' ]
+
+    choice = st.sidebar.selectbox('메뉴', menu)
+
+    
+    if choice == 'HOME' :
+        st.header('주가 예측 앱')
+        st.write('KOSPI , KOSDAQ 시세 및 종목 분석 예측')
+        st.write('2000년 이후')
+        st.header('''
+    
+                ''')
+    
+        img = Image.open('data/pic.jpg')
+        st.image(img)
+    
+
+    
+    if choice == 'KOSPI & KOSDAQ' :
+        run_kospi()
+
+    if choice == 'DJI & NASDAQ' :
+        run_america()
+
+    if choice == '종목검색':
+        run_search()
+        
+
+    
+
+
+
+
+
+if __name__ == '__main__' :
+    main()
